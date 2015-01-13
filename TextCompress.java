ccf4b41725471e7cef38ecb812a427f38e620efa
Konstantinos Fronistas <konfron@users.noreply.github.com>
2015-01-13 12:02:54 +0200

    method decompress - text

--- TextCompress.java
+++ TextCompress.java
@@ -8,6 +8,52 @@ public class TextCompress
 	
 	public static void decompress(File input,File output)
 	{
+		try 
+		{
+			CharsetEncoder encoder = Charset.forName("UTF-8").newEncoder();
+			
+			FileOutputStream fos = new FileOutputStream(output);
+			Writer out = new OutputStreamWriter(fos, encoder);
+			
+			FileInputStream fis = new FileInputStream(input);
+	        InputStreamReader isr = new InputStreamReader(fis, encoder.charset());
+	        Reader fin = new BufferedReader(isr);
+			
+			int curr=0;
+			int prev=-1;
+			
+			while( (curr = fin.read()) != -1 )
+            {
+				if(curr==prev)
+				{
+					prev=curr;
+					if((curr = fin.read()) != -1 )
+					{
+						int times=Character.getNumericValue(curr);
+						for(int i=0;i<times+1;i++)
+						{
+							out.write(prev); 
+						}
+					}
+					prev=-1;
+					continue;
+				}
+				else
+				{
+					out.write(curr);
+					prev=curr;
+					continue;
+				}
+            }
+			
+			fin.close();
+			out.close();
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+			
+		} 
 	}
 	
 	public static void write2output(int runLength,Writer out,int currbyte)
