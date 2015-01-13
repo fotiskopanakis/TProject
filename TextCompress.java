dcbb2f25342bdec2f166b1a7ac337267645582b7
harisbitsakou <harisbitsakou@hotmail.com>
2015-01-13 12:01:19 +0200

    txtcompress - write2output method

--- TextCompress.java
+++ TextCompress.java
@@ -9,4 +9,25 @@ public class TextCompress
 	public static void decompress(File input,File output)
 	{
 	}
+	
+	public static void write2output(int runLength,Writer out,int currbyte)
+	{
+		try 
+		{
+			if(runLength==1)
+			{
+				out.write(currbyte);
+			}
+			else if(runLength>1)
+			{
+				out.write(currbyte);
+				out.write(currbyte);
+				out.write(String.valueOf(runLength-2));
+			}
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+	}
 }
